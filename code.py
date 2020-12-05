23064280b5d53d758995609e5076a343ea87f669
anushkawakankar <anushka.wakankar@gmail.com>
2020-12-05 18:07:23 +0530

    added fps and loop functionality

--- code.py
+++ code.py
@@ -3,23 +3,71 @@ import os
 import cv2  
 from PIL import Image 
 import numpy as np 
+import imutils
+
+def resizeImages(mw, mh, imageNamesUnique):
+    for im in imageNamesUnique:
+        image = cv2.imread(im)
+        newImage = imutils.resize(image , width=mw, height=mh)
+        cv2.imwrite(im, newImage)
+
+def generateVideo(imageNames,fpsUser,loop):
+    video_name = "myvideo.avi"
+    frame = cv2.imread(imageNames[0])
+    height, width, layers = frame.shape  
+    video = cv2.VideoWriter(video_name, 0, fpsUser, (width, height))
+
+    for _ in range(loop):
+        for image in imageNames:  
+            video.write(cv2.imread(image)) 
+    cv2.destroyAllWindows()  
+    video.release()
+
+
+filename = "commands.flip"
 
 # reading the language file
-f = open("commands.flip","r")
-imageNames = []
-coord = np.zeros((1000,2))
 
+if(not os.path.exists(filename)):
+    print("Error:",filename,"does not exist")
+    exit(0)
+f = open(filename,"r")
+imageNames = []
+imageNamesUnique = []
+coord = np.zeros((1000,2))
+fpsUser = 15
+loop = 3
 Lines = f.readlines()
 i = 1
 for line in Lines:
+    if("fps" in line):
+        args_fps = line.split('=')
+        # print(args_fps)
+        x = args_fps.index("fps")
+        # print(x)
+        fpsUser = int(args_fps[x+1].strip())
+        # print(fpsUser)
+        continue
+    
+    if("loop" in line):
+        args_loop = line.split('=')
+        x = args_loop.index("loop")
+        loop = int(args_loop[x+1].strip())
+        continue
+    
     args = line.split(' ')
     if(len(args) != 3 and len(args) != 5):
-        print("Error in line ",i,". Format is {start frame} {end frame} {x_index} {y_index} {filename}\n")
+        print("Error in line "+str(i)+". Format is {start frame} {end frame} {x_index} {y_index} {filename}")
+        print("or {start frame} {end frame} {filename}")
         exit(0)
     else:
         # i += 1
         sf = int(args[0])
         ef = int(args[1])
+        if(not isinstance(sf, int) or not isinstance(ef, int)):
+            print("Error in line "+str(i)+". Format is {start frame} {end frame} {x_index} {y_index} {filename}")
+            print("or {start frame} {end frame} {x_index} {y_index} {filename}")
+            exit(0)
         x = 0
         y = 0
         image = ""
@@ -32,13 +80,36 @@ for line in Lines:
         image = image.strip()
         
         if(not os.path.exists(image)):
-            print("Error in line",i,". ",image," does not exist.\n")
+            print("Error in line"+str(i)+". "+image+" does not exist.")
             exit(0)
         
         numberOfFrames = ef-sf
+        imageNamesUnique.append(image)
         for i in range(0,numberOfFrames):
             imageNames.append(image)
         
         i += 1
-print(imageNames)
+# print(imageNames)
 
+# Constructing the image list for the gif
+
+# Making all images the same size
+mean_height = 0
+mean_width = 0
+num_of_images = len(imageNamesUnique)
+
+for impath in imageNamesUnique: 
+    im = Image.open(impath) 
+    width, height = im.size 
+    mean_width += width 
+    mean_height += height
+
+mean_width = int(mean_width / num_of_images) 
+mean_height = int(mean_height / num_of_images)
+
+# Resize images to make the video seamless
+resizeImages(mean_width, mean_height, imageNamesUnique)
+
+# Generate video
+# print(fpsUser,loop)
+generateVideo(imageNames, fpsUser, loop)
